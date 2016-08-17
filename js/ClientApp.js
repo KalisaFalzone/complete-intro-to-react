8cdfa1e53c658246efe1902708a028eb217da485
KalisaFalzone <kalisa.falzone@gmail.com>
2016-08-17 14:37:46 -0700

    end of props

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -5,7 +5,7 @@ var MyTitle = React.createClass({
   render () { // same as render: function ()...
     return (
       div(null,
-        h1(null, 'Check out this other thing.')
+        h1({style: {color: this.props.color}}, this.props.title)
       )
     )
   }
@@ -17,9 +17,9 @@ var ce = React.createElement
 var MyFirstComponent = (
   div(null,
     //these three lines work the same the last two are just shorter hand
-    React.createElement(MyTitle, null),
-    MyTitleFact(null),
-    ce(MyTitle, null)
+    React.createElement(MyTitle, {title: 'Use props everywhere', color: 'papayawhip'}),
+    MyTitleFact({title: 'Props are great', color: 'rebeccapurple'}),
+    ce(MyTitle, {title: 'Props are the best!', color: 'mediumaquamarine'})
   )
 )
 ReactDOM.render(MyFirstComponent, document.getElementById('app'))
